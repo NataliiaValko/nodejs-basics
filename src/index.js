fa54e15ad1f669394bc441e35301b3057974eae5
NataliiaValko <tanarisoffice@gmail.com>
2024-05-05 23:42:06 +0300

    m02b01 Початковий код для 'Файлова структура застосунку' screen

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
+// import express from 'express';
+// import pino from 'pino-http';
+// import cors from 'cors';
 
-const PORT = 3000;
+// const PORT = 3000;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
+// app.use(
+//   pino({
+//     transport: {
+//       target: 'pino-pretty',
+//     },
+//   }),
+// );
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello world!',
+//   });
+// });
 
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
-  });
-});
+// app.use('*', (req, res, next) => {
+//   res.status(404).json({
+//     message: 'Not found',
+//   });
+// });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
+// app.use((err, req, res, next) => {
+//   res.status(500).json({
+//     message: 'Something went wrong',
+//     error: err.message,
+//   });
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
